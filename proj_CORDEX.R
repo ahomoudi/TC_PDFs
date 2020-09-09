9abd511a593baf28fe6c02fc25722eeec84e44ab
ahmathlete <Ahmed.a.b.Homoudi@gmx.de>
2020-09-09 15:58:42 +0200

    project_CORDEX

---
+++ proj_CORDEX.R
@@ -0,0 +1,47 @@
+# function  to read opened  COREDEX netcdf file and projected to global CRS + crop it 
+
+proj_read_toff<- function(netcdf_file){
+	library(ncdf4)
+	library(stringr)
+	library(ff)
+	
+	ncin <- nc_open(netcdf_file)
+	
+	file_name<-ncin[["filename"]]
+	
+	variable_name<- paste0(ncin[["var"]][[2]][["name"]],"_",
+		   strsplit(unlist(str_split(file_name,
+		   	      "_"))[9],     #get year of the data 
+		            "[.]")[[1]][1])
+
+	nc.var<- ncin[["var"]][[2]][["name"]]		#varaible name 
+	
+	
+	first_command<-paste0("cdo -remapbil,proj_WAS.txt ",        #cdo command 
+		  file_name," ",
+		  output_file)
+		
+	output_file<- paste0(unlist(strsplit(file_name,"[.]"))[1],  #projected file name
+		 "_tmp.nc")
+	
+	system(first_command)		# call cdo in the system 
+	
+	ncin_cropped<- nc_open(output_file)		#open netcdf file 
+	
+	med<- ncvar_get(ncin_cropped,nc.var)		#obtain variable from netcdf file 
+	
+	lon <-ncvar_get(ncin_cropped,"longitude")
+	lat <- ncvar_get(ncin_cropped,"latitude")
+	time <- ncvar_get(ncin_cropped,"time")
+	
+	processed_array<- ff(med, dim= dim(med),
+		 dimnames= list(lon,lat,time))	# save data as ff object
+	
+	system(paste0("rm ",output_file))		#remove projected netcdf file 
+	
+	return(processed_array)
+	
+}
+#netcdf_file<-nc.files[i]
+
+
